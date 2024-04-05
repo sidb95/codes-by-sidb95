799ffeb2e32b8602d7b2937bdcda6d0942c8032a
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-04-05 21:47:47 +0530

    Update: to be continued...

---
+++ test_factorial_sum_one.h
@@ -0,0 +1,13 @@
+// 0! + 1! + 2! + ... + n!
+/*
+sidb95
+bhatoresiddharth@gmail.com
+test_factorial_sum_one
+05.04.2024
+*/
+
+#include "factorial_sum.h"
+
+short int testFactorialSumOne(int n, int calcAnswer) {
+}
+
