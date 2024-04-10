587812574bb6e93edcddec9b13bc1588e97dc60a
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-04-10 17:04:40 +0530

    Update selection_sort.h

--- selection_sort.h
+++ selection_sort.h
@@ -4,3 +4,19 @@ bhatoresiddharth@gmail.com
 selection_sort
 10.04.2024
 */
+
+int* selectionSort(int* numArr, int n, int* minNums) {
+    int indexMin = 0;
+    minNums[0] = EOF;
+    int j=0;
+    while(j != n) {
+        for(int i=0; i<n; i++) {
+            if(numArr[i] < numArr[indexMin]) {
+            indexMin = i;
+        }
+        minNums[j] = numArr[indexMin];
+        j++;
+    }
+    minNums[j] = EOF;
+    return minNums;
+}
