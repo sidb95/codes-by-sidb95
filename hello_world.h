952dad8f25c5daa279ed87f6b6a463d015d12ec5
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-04-10 15:33:10 +0530

    Update hello!

---
+++ hello_world.h
@@ -0,0 +1,12 @@
+/*
+sidb95
+bhatoresiddharth@gmail.com 
+hello_world
+10.04.2024
+*/
+
+void helloWorld(char* strArr) {
+    strArr = {'h', 'e', 'l', 'o', '!' 'w', 'r', 'd'};
+    printf("%c%c%c%c%c", strArr[0], strArr[1], strArr[2], strArr[2], strArr[3], strArr[4]);
+    return;
+}
