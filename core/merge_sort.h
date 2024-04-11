712fd333f021b479c2b6310a136397bad9fc42d3
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-04-11 21:37:02 +0530

    Update merge_sort.h

--- core/merge_sort.h
+++ core/merge_sort.h
@@ -5,17 +5,43 @@ merge_sort
 11.04.2024
 */
 
-// n : int, >= 1
+// n : int, n >= 1
 
 int* mergeSortAux(int* numArr, int n, int start, int end, int* retArr) {
     retArr[0] = numArr[start];
+    int count = 1;
+    int mid = (start+(end-start)/2);
     start++;
     if(start >= end) {
          return retArr;
     }
-    int i;
-    for(int i=start; i<
-    
+    int i=start, j=mid+1;
+    for(int i=start; i<=mid; i++) {
+        if(numArr[i] > numArr[j]) {
+            retArr[count] = numArr[j];
+            j++;
+        }
+        else {
+            retArr[count] = numArr[i];
+            i++;
+        }
+        count++;
+    }
+    if(count != n) {
+        while(count != n) {
+            if(i == mid) {        
+                retArr[count] = numArr[i];
+                count++;
+                i++;
+            }
+            else {
+                retArr[count] = numArr[j];
+                count++;
+                j++;
+            }
+        }
+    }
+    return retArr;
 }
 
 int* mergeSort(int* numArr, int n, int start, int end) {
@@ -25,7 +51,8 @@ int* mergeSort(int* numArr, int n, int start, int end) {
         return numArr;
     }
     else {
-        mergeSort(mergeSortAux(numArr, n, start, start+(end-start)/2, retArr), mergeSortAux((start+(end-start)/2) + 1, end), retArr);
+        int mid = (start+(end-start)/2);
+        mergeSort(mergeSortAux(numArr, n, start, mid, retArr), mergeSortAux(mid + 1, end), retArr);
     }
     return retArr;
 }
