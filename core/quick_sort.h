ff4ada64b488bca36587287a540e88bb6c9bffd5
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-04-12 23:50:40 +0530

    quick_sort
    a simple recursive quick sort algorithm

--- core/quick_sort.h
+++ core/quick_sort.h
@@ -5,6 +5,18 @@ quick_sort
 12.04.2024
 */
 
-int* quickSort(int* numArr, int n, int start, int end, int* retArr) {
-    retArr = (int*)malloc(sizeof(int)*n);
+#include <stdlib.h>
+#include <malloc.h> 
+
+int* quickSort(int* numArr, int n, int start, int end) {
+    int pivot=n-1, i=n-2, tmpVar;
+    while (i >= 0 && numArr[pivot] < numArr[i]) {
+        tmpVar = numArr[pivot];
+        numArr[i] = tmpVar;
+        tmpVar = numArr[i];
+        pivot--;
+    }
+    quickSort(numArr, pivot-start+1, start, pivot);
+    quickSort(numArr, end-pivot+1, pivot+1, end);
+    return numArr;
 }
