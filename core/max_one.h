caa0817e2cbb79d92a27068db01ef8e60420e7ec
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-04-19 02:55:22 +0530

    update
    min and max if two ints.

---
+++ core/max_one.h
@@ -0,0 +1,10 @@
+/*
+sidb95
+bhatoresiddharth@gmail.com
+max_one
+17.04.2024
+*/
+
+int maxOne(int a, int b) {
+    return ((a > b) ? (a : b));
+}
---
+++ core/min_one.h
@@ -0,0 +1,10 @@
+/*
+sidb95
+bhatoresiddharth@gmail.com
+min_one
+17.04.2024
+*/
+
+int minOfTwo(int a, int b) {
+    return ((a < b) ? (b : a));
+}
