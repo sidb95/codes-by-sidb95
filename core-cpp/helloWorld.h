8398f839480a7c17db441cafab5fbb166f0d5ff8
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-04-18 20:06:44 +0530

    Update core-cpp

---
+++ core-cpp/helloWorld.h
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+    public:
+    string varStr = "hello world!";
+    void printTag() {
+        cout << varStr << endl;
+        return;
+    }
+};
