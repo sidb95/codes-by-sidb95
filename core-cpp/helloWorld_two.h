c76e44eac8adfd7a78a54ebf4992dbcf91d02aad
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-04-24 08:54:13 +0530

    Update core-cpp

---
+++ core-cpp/helloWorld_two.h
@@ -0,0 +1,20 @@
+/*
+sidb95
+bhatoresiddharth@gmail.com 
+helloWorld_two
+24.04.2024
+*/
+
+#include<iostream>
+using namespace std;
+
+class Solution {
+    public:
+    
+    void helloWorldTwo(string varStr) {
+        cout << varStr << endl;
+        return;
+    }
+}
+
+
