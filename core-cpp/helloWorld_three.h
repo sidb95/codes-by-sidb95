6157ab06d35ecf16ae40404af0ec685c6dc0f189
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-05-12 13:34:12 +0530

    Update core-cpp

---
+++ core-cpp/helloWorld_three.h
@@ -0,0 +1,20 @@
+/*
+helloWorld_three
+bhatoresiddharth@gmail.com 
+sidb95
+12 May 2024
+*/
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+private:
+    strVarA = "Hello World";
+public:
+    Solution() {
+        cout << strVarA << endl;
+    }
+};
+
