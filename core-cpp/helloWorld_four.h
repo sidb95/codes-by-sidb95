aa555673441a55289def81b4fb6e9892e8863e95
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-05-14 12:48:34 +0530

    Update core-cpp, add helloWorld_four

---
+++ core-cpp/helloWorld_four.h
@@ -0,0 +1,36 @@
+/*
+helloWorld_four
+bhatoresiddharth@gmail.com 
+sidb95
+14 May 2024
+*/
+
+#include <iostream>
+#include <string>
+
+class Solution {
+private:
+    string myStr = "sidb95";
+    void changeStr(string& varStr) {
+        myStr = varStr;
+        return;
+    }
+public:
+    Solution(string str="1) ") {
+        string varStr1 = myStr;
+        str += varStr1;
+        changeStr(str)
+    }
+    void printFunc() {
+        cout << myStr << endl;
+        return;
+    }
+};
+
+class Main::Solution {
+public:
+    Main() {
+        Solution mySol = new Solution("");
+        mySol.printFunc()
+    }
+};
