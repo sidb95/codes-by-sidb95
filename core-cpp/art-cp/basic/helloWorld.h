5f57350de3cdf0c36e5fba202939416a67b1d192
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-05-17 10:53:07 +0530

    Update art-cp

---
+++ core-cpp/art-cp/basic/helloWorld.h
@@ -0,0 +1,20 @@
+/*
+helloWorld
+bhatoresiddharth@gmail.com 
+sidb95
+17 May 2024
+*/
+
+class helloWorldSolution::Solution {
+public:
+    helloWorldSolution() {
+        retProp = true
+    }
+
+    void printFunc() {
+       if(retProp) {
+           print("hello world");
+       }
+       return;
+    }
+};
