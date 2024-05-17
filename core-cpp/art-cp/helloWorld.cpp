09bcb87c249e1995412041f03475d22afda482b7
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-05-17 10:39:36 +0530

    Update art-cp

---
+++ core-cpp/art-cp/helloWorld.cpp
@@ -0,0 +1,18 @@
+/*
+bhatoresiddharth@gmail.com 
+helloWorld
+sidb95
+17 May 2024
+*/
+
+#include<iostream>
+#include "Solution.h"
+#include "helloWorld.h"
+
+using namespace std;
+
+int main() {
+    helloWorldSolution mySol1;
+    mySol1.printFunc();
+    return 0;
+}
