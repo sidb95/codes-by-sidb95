051a76e07825ad96e8be510eb9dbb90ef8532b47
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-05-17 11:06:05 +0530

    Update core

---
+++ core-cpp/art-cp/core/Solution.h
@@ -0,0 +1,18 @@
+/*
+Solution
+bhatoresiddharth@gmail.com 
+sidb95
+17 May 2024
+*/
+
+class Solution {
+private:
+    bool retProp;
+public:
+    Solution() {
+        retProp = false;
+    }
+    void print(string str) {
+        cout << str << endl;
+    }
+};
