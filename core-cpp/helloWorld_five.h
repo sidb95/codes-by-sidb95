e72be4f97e26a722be2ac3348f29e8526554598b
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-05-19 10:11:03 +0530

    Update helloWorld_five.h

--- core-cpp/helloWorld_five.h
+++ core-cpp/helloWorld_five.h
@@ -4,3 +4,18 @@ bhatoresiddharth@gmail.com
 sidb95
 19 May 2024
 */
+
+#include "art-cp/core/Solution.h"
+
+class helloWorld::Solution {
+private:
+    string str;
+public:
+    helloWorld() {
+        str = "Hello World!"
+    }
+    printFunc() {
+        cout << str << endl;
+        return;
+    }
+}
