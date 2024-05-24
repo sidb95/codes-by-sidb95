f8c0305c488a4ea55a3a13e8a0bbe55aa0f354be
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-05-24 14:52:16 +0530

    Update print

---
+++ core-cpp/print/helloWorld_six.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std
+
+string STR = "Hello World!";
+
+void printFunc();
+
+void printFunc() {
+    std::cout << STR << endl;
+    return;
+}
