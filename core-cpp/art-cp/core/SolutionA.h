1e177c28024f20bd63b401cd872884d63445842f
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-06-06 19:53:45 +0530

    Update SolutionA.h

--- core-cpp/art-cp/core/SolutionA.h
+++ core-cpp/art-cp/core/SolutionA.h
@@ -4,3 +4,13 @@ bhatoresiddharth@gmail.com
 sidb95
 25 May 2024
 */
+# include <iostream>
+
+using namespace std;
+
+class SolutionA {
+public:
+    void printString(string str) {
+        cout << str << endl;
+    }
+};
