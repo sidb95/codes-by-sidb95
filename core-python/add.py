eb914b0aacf9e7f4e113a3463081bb262f023172
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-01 15:54:34 +0530

    Update print.py

--- core-python/add.py
+++ core-python/add.py
@@ -1,1 +1,29 @@
+import random
+
+def print:
+  def __init__():
+    self.FLAG = True
+
+  def printFunc(str1: str):
+    print(str1)
+    return
+
+def __main__():
+  P = print()
+  S = ('H', 'E', 'L', 'O', 'W', 'R', 'D')
+  arr = ['H', 'E', 'L', 'L', 'O', ' ']
+  vis = []
+  count = 0
+  while (count != 5):
+    i = rand.randint(0, 4) + 2
+    if (S[i] not in vis):
+      arr.append(S[i])
+      vis.append(S[i])
+      count += 1
+  str1 = "Hello World"
+  strA = ""
+  for elmt in arr:
+    strA += elmt
+  P.printFunc(str1)
+  P.printFunc(strA)
 
