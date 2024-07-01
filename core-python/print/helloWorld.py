9049c7f3fa376c36021f58fd82d80d521f6b13c0
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-01 18:04:57 +0530

    Update helloWorld.py

--- core-python/print/helloWorld.py
+++ core-python/print/helloWorld.py
@@ -1,1 +1,18 @@
+import random
 
+def __main__():
+  P = print()
+  S = ('H', 'E', 'L', 'O', 'W', 'R', 'D')
+  arr = ['H', 'E', 'L', 'L', 'O', ' ']
+  vis = []
+  count = 0
+  while (count != 5):
+    i = rand.randint(0, 4) + 2
+    if (S[i] not in vis):
+      arr.append(S[i])
+      vis.append(S[i])
+      count += 1
+  c1 = ''
+  strA = c1.join(S)
+  P.printFunc(strA)
+  P.printFunc(arr)
