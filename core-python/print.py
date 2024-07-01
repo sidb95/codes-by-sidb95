e9a5484d585fab8e591b78173d7f7152710f6c1a
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-01 18:02:53 +0530

    Update print.py

--- core-python/print.py
+++ core-python/print.py
@@ -8,23 +8,3 @@ class Print:
     if (self.FLAG):
       print(str1)
     return None
-
-def __main__():
-  P = print()
-  S = ('H', 'E', 'L', 'O', 'W', 'R', 'D')
-  arr = ['H', 'E', 'L', 'L', 'O', ' ']
-  vis = []
-  count = 0
-  while (count != 5):
-    i = rand.randint(0, 4) + 2
-    if (S[i] not in vis):
-      arr.append(S[i])
-      vis.append(S[i])
-      count += 1
-  str1 = "Hello World"
-  strA = ""
-  for elmt in arr:
-    strA += elmt
-  P.printFunc(str1)
-  P.printFunc(strA)
-
