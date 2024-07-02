73aad1f7cf17a13ac53d8b744bcfafcf40f9f916
= <=>
2024-07-02 12:05:03 +0530

    Update helloWorld.py

--- core-python/print/helloWorld.py
+++ core-python/print/helloWorld.py
@@ -1,13 +1,14 @@
 import random
+import print
 
 def __main__():
-  P = print()
+  P = print.Print()
   S = ('H', 'E', 'L', 'O', 'W', 'R', 'D')
   arr = ['H', 'E', 'L', 'L', 'O', ' ']
   vis = []
   count = 0
   while (count != 5):
-    i = rand.randint(0, 4) + 2
+    i = random.randint(0, 4) + 2
     if (S[i] not in vis):
       arr.append(S[i])
       vis.append(S[i])
