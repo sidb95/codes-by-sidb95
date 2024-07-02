01e3d217fa985d1355a6b0a36d2d1f6eaf9c62b7
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-02 15:05:16 +0530

    helloWorld.py

--- core-python/number-theory/helloWorld.py
+++ core-python/number-theory/helloWorld.py
@@ -1,1 +1,29 @@
+import random
+import print
 
+class Print:
+  def __init__(self):
+    pass
+
+  def printFunc(self, str1: str):
+    print(str1)
+
+def __main__():
+  t = input()
+  T = int(t)
+  for t in range(0, T):
+    P = print.Print()
+    S = ('H', 'E', 'L', 'O', 'W', 'R', 'D')
+    arr = ['H', 'E', 'L', 'L', 'O', ' ']
+    vis = []
+    count = 0
+    while (count != 5):
+      i = random.randint(0, 4) + 2
+      if (S[i] not in vis):
+        arr.append(S[i])
+        vis.append(S[i])
+        count += 1
+    c1 = ''
+    P.printFunc(c1.join(arr))
+
+__main__()
