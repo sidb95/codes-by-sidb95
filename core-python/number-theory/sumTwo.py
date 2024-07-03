695f29ae9bedc48b31f4352c94a69dfc1e1189c5
= <=>
2024-07-03 14:29:40 +0530

    Update sumTwo.py

--- core-python/number-theory/sumTwo.py
+++ core-python/number-theory/sumTwo.py
@@ -1,2 +1,18 @@
+import add
+
+class Print:
+  def __init__(self):
+    pass
+
+  def printFunc(self, str1: str):
+    print(str1)
+
 def sumTwoNums(a: int, b: int) -> int:
-  pass
+  A = add.Add()
+  P = Print()
+  P.printFunc(A.sumTwoNums(a, b))
+  return
+
+def __main__():
+  sumTwoNums(1, 0)
+  return
