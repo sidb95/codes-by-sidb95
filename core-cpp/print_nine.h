b8879918d70280c037fb80d217bb7fc974a124bb
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-09 06:24:17 +0530

    Update print_nine.h

--- core-cpp/print_nine.h
+++ core-cpp/print_nine.h
@@ -4,7 +4,6 @@ sidb95
 bhatoresiddharth@gmail.com
 09 July 2024
 */
-
 #include "print.h"
 
 class Solution() {
@@ -18,5 +17,11 @@ public:
     FLAG = false;
   }
 
-  void 
+  void printFunc(string strA) {
+    string retStr = "";
+    for (int i = 0; i < strA.size(); i += 1)
+      printString(strA[i]);
+    printString("\n");
+    return;
+  }
 };
