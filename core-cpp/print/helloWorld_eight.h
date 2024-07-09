b4563f7c94605b0eef5b3b01f68560a7d3390739
Siddharth Bhatore <bhatoresiddharth@gmail.com>
2024-07-09 06:35:04 +0530

    Update helloWorld_eight.h

--- core-cpp/print/helloWorld_eight.h
+++ core-cpp/print/helloWorld_eight.h
@@ -4,3 +4,25 @@ sidb95
 bhatoresiddharth@gmail.com
 04 July 2024
 */
+
+#include "print.h"
+
+class Solution {
+public:
+    FLAG = false;
+    Solution() {
+        FLAG = true;
+    }
+
+    ~Solution() {
+        FLAG = false;
+    }
+
+    void printFunc(string strA) {
+       int size1 = strA.size();
+       for (int i = 0; i < size1; i += 1) {
+           printString(strA[size1 - 1 - i]);   
+       }
+      return;
+    }
+};
